75c135c62891acbc5f35fd8aa9ba020d6f06b30d
Milka Chasi <mchasijimenez@gmail.com>
2025-05-24 21:13:54 -0500

    Tarea 7 Suma de elementos en una lista con for

---
+++ python_basico_siete.py
@@ -0,0 +1,9 @@
+#  Suma de elementos en una lista con for
+#  Crea una lista de números y úsala para calcular la suma total.
+
+suma_total = 0
+for lista in range(0, 20):
+    suma_total += lista
+    print(lista)
+
+print("La suma total es:", suma_total)
