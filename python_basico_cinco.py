718bf1b31c836ca05ef2708bf1f4f1a8a826d77f
Milka Chasi <mchasijimenez@gmail.com>
2025-05-24 20:12:25 -0500

    Tarea 5 Cuenta regresiva con while

---
+++ python_basico_cinco.py
@@ -0,0 +1,7 @@
+# Escribe un programa que pida un número y haga una cuenta regresiva hasta 0 usando while.
+
+numero = int(input("Ingresa un número: "))
+
+while numero >= 0:
+    print(numero)
+    numero -= 1
