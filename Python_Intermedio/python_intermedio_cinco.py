8ebf8fae0260dea64f7afe6e50aa92b77bb0e551
Milka Chasi <mchasijimenez@gmail.com>
2025-06-02 17:37:19 -0500

    Tarea 5 "Tuplas"

---
+++ Python_Intermedio/python_intermedio_cinco.py
@@ -0,0 +1,9 @@
+# Dada la tupla punto=(7, 3), extrae sus coordenadas a dos variables y súmalas.
+
+punto = (7, 3)
+
+x, y = punto
+
+suma = x + y
+
+print(f" las coordenadas son 'x': {x} y 'y': {y}. Su suma es igual a {suma}")
