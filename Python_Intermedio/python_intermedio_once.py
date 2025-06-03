6cf6e21f87238cdee4a3b0279092168087532df6
Milka Chasi <mchasijimenez@gmail.com>
2025-06-03 16:26:13 -0500

    Tarea 11 "Sets"

---
+++ Python_Intermedio/python_intermedio_once.py
@@ -0,0 +1,12 @@
+# Dados A = {1,2,3} y B = {3,4}, calcula A∪B, A∩B y B−A.
+A = {1, 2, 3}
+B = {3, 4}
+
+union = A | B
+print(union)
+
+interseccion = A & B
+print(interseccion)
+
+diferenccia =  A - B
+print(diferenccia)
