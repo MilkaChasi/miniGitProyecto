9ebb5865ddc5cf28cd033d86201c6552d8e6721c
Milka Chasi <mchasijimenez@gmail.com>
2025-06-03 13:56:51 -0500

    Tarea 7  "Diccionarios"

---
+++ Python_Intermedio/python_intermedio_siete.py
@@ -0,0 +1,12 @@
+# Representa una lista de contactos donde la clave sea el nombre y el valor el número de teléfono.
+
+contactos = {
+    "Andres": "09655702414",
+    "Carlos": "09965702418",
+    "Jhonn": "09655702514",
+    "Luis": "09714705813",
+    "Pedro": "09185632110",
+    "Juan": "09135755412",
+}
+
+print(contactos)
