d1561e85eddc9e07ab2e16950c6c982786e24c3c
Milka Chasi <mchasijimenez@gmail.com>
2025-06-03 14:06:58 -0500

    Tarea 8  "Diccionarios"

---
+++ Python_Intermedio/python_intermedio_ocho.py
@@ -0,0 +1,6 @@
+# Dado el diccionario inventario = {"manzanas": 10, "naranjas": 5}, incrementa el valor de "manzanas" en 20%.
+
+inventario = {"manzanas": 10, "naranjas": 5}
+inventario["manzanas"] = int(inventario["manzanas"] * 1.20)
+
+print(inventario)
