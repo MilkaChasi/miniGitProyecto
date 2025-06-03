be049c84a43eff458099242ca4c14ba7c59ab5bb
Milka Chasi <mchasijimenez@gmail.com>
2025-06-03 16:34:44 -0500

    Tarea 12 "Sets"

---
+++ pyhon_intermedio_doce.py
@@ -0,0 +1,6 @@
+# A partir de la lista ['a','b','a','c','b','d'], genera un set para quedarse solo con los elementos únicos.
+
+lista = ["a", "b", "a", "c", "b", "d"]
+sets = set(lista)
+print(type(sets))
+print(sets)
