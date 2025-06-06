e7393b52669eee781193779ba7cf84ba1b55cc3d
Milka Chasi <mchasijimenez@gmail.com>
2025-06-06 14:12:57 -0500

    Tarea 23 Modularización

---
+++ operaciones.py
@@ -0,0 +1,45 @@
+"""Módulo operaciones: contiene funciones básicas de suma, resta, multiplicación y división."""
+
+
+def multiplicaciones(x, n):
+    """
+    División.
+
+    Parámetros:
+    x (int): Número entero a evaluar.
+    n (int): Número entero a evaluar.
+    """
+    return x * n
+
+
+def division(x, n):
+    """
+    Multiplicación.
+
+    Parámetros:
+    x (int): Número entero a evaluar.
+    n (int): Número entero a evaluar.
+    """
+    return x / n
+
+
+def suma(x, n):
+    """
+    Suma.
+
+    Parámetros:
+    x (int): Número entero a evaluar.
+    n (int): Número entero a evaluar.
+    """
+    return x + n
+
+
+def resta(x, n):
+    """
+    Resta.
+
+    Parámetros:
+    x (int): Número entero a evaluar.
+    n (int): Número entero a evaluar.
+    """
+    return x - n
---
+++ operaciones_aplicadas.py
@@ -0,0 +1,7 @@
+import operaciones
+
+
+print(operaciones.suma(4, 5))
+print(operaciones.resta(8, 5))
+print(operaciones.multiplicaciones(4, 5))
+print(operaciones.division(5, 5))
